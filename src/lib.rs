b53c4cb5a8913403b22e31afccbeb868367f46c9
ACK72 <acikom72@gmail.com>
2023-08-18 17:55:30 +0900

    style: remove unnecessary clone

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ fn get_kavita_api_auth() -> String {
 	if unsafe { KAVITA_API_AUTH.is_empty() } {
 		let kavita_api_url = get_kavita_api_url();
 		let kavita_api_key = get_kavita_api_key();
-		let request_url = format!("{}/Plugin/authenticate?apiKey={}&pluginName=Kavya", kavita_api_url.clone(), kavita_api_key.clone());
+		let request_url = format!("{}/Plugin/authenticate?apiKey={}&pluginName=Kavya", kavita_api_url, kavita_api_key);
 
 		let response = Request::post(encode_uri(&request_url)).json().unwrap();
 		let auth = response.as_object().unwrap().get("token").as_string().unwrap().to_string();
@@ -39,7 +39,7 @@ fn get_kavita_api_auth() -> String {
 		}
 	}
 
-	unsafe { format!("Bearer {}", KAVITA_API_AUTH.clone()) }
+	unsafe { format!("Bearer {}", KAVITA_API_AUTH) }
 }
 
 #[get_manga_list]
@@ -65,11 +65,11 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 	let request_body;
 
 	if query.is_empty() {
-		request_url = format!("{}/Series/all?PageNumber={}&PageSize={}", kavita_api_url.clone(), page, 40);
+		request_url = format!("{}/Series/all?PageNumber={}&PageSize={}", kavita_api_url, page, 40);
 		request_body = "{}".to_string();
 	} else {
 		// Cannot use /api/Search/search?queryString API, seems have an internal error
-		request_url = format!("{}/Series?PageNumber={}&PageSize={}", kavita_api_url.clone(), page, 40);
+		request_url = format!("{}/Series?PageNumber={}&PageSize={}", kavita_api_url, page, 40);
 		request_body = serde_json::json!({
 			"seriesNameQuery": query
 		}).to_string();
@@ -92,12 +92,12 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 	let mut result = Vec::new();
 	for series_object in response.as_array().unwrap() {
 		let series = series_object.as_object().unwrap();
-		let id = series.clone().get("id").as_int().unwrap().to_string();
+		let id = series.get("id").as_int().unwrap().to_string();
 		let title = series.get("name").as_string().unwrap().to_string();
 
 		result.push(Manga {
 			id: id.clone(),
-			cover: format!("{}/image/series-cover?seriesId={}&apiKey={}", kavita_api_url.clone(), id, kavita_api_key.clone()),
+			cover: format!("{}/image/series-cover?seriesId={}&apiKey={}", kavita_api_url, id, kavita_api_key),
 			title: title,
 			..Default::default()
 		});
@@ -118,15 +118,15 @@ fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
 	let mut key_id = "id";
 	let mut key_title = "name";
 	let request_url = match listing.name.as_str() {
-		"On Deck" => format!("{}/Series/on-deck?PageNumber={}&PageSize={}", kavita_api_url.clone(), page, 40),
+		"On Deck" => format!("{}/Series/on-deck?PageNumber={}&PageSize={}", kavita_api_url, page, 40),
 		"Recently Updated" => {
 			key_id = "seriesId";
 			key_title = "seriesName";
-			format!("{}/Series/recently-updated-series", kavita_api_url.clone())
+			format!("{}/Series/recently-updated-series", kavita_api_url)
 		},
-		"Newly Added" => format!("{}/Series/recently-added?PageNumber={}&PageSize={}", kavita_api_url.clone(), page, 40),
-		"Want To Read" => format!("{}/want-to-read?PageNumber={}&PageSize={}", kavita_api_url.clone(), page, 40),
-		_ => format!("{}/Series/all?PageNumber={}&PageSize={}", kavita_api_url.clone(), page, 40)
+		"Newly Added" => format!("{}/Series/recently-added?PageNumber={}&PageSize={}", kavita_api_url, page, 40),
+		"Want To Read" => format!("{}/want-to-read?PageNumber={}&PageSize={}", kavita_api_url, page, 40),
+		_ => format!("{}/Series/all?PageNumber={}&PageSize={}", kavita_api_url, page, 40)
 	};
 	
 	let request = Request::post(encode_uri(&request_url))
@@ -146,12 +146,12 @@ fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
 	let mut result = Vec::new();
 	for series_object in response.as_array().unwrap() {
 		let series = series_object.as_object().unwrap();
-		let id = series.clone().get(key_id).as_int().unwrap().to_string();
+		let id = series.get(key_id).as_int().unwrap().to_string();
 		let title = series.get(key_title).as_string().unwrap().to_string();
 
 		result.push(Manga {
 			id: id.clone(),
-			cover: format!("{}/image/series-cover?seriesId={}&apiKey={}", kavita_api_url.clone(), id, kavita_api_key.clone()),
+			cover: format!("{}/image/series-cover?seriesId={}&apiKey={}", kavita_api_url, id, kavita_api_key),
 			title: title,
 			..Default::default()
 		});
@@ -172,7 +172,7 @@ fn get_manga_details(manga_id: String) -> Result<Manga> {
 	let kavita_api_url = get_kavita_api_url();
 	let kavita_api_key = get_kavita_api_key();
 
-	let series_url = format!("{}/Series/{}", kavita_api_url.clone(), manga_id.clone());
+	let series_url = format!("{}/Series/{}", kavita_api_url, manga_id);
 	let series_req = Request::get(encode_uri(&series_url))
 		.header("Authorization", get_kavita_api_auth().as_str());
 
@@ -184,7 +184,7 @@ fn get_manga_details(manga_id: String) -> Result<Manga> {
 		});
 	}
 
-	let metadata_url = format!("{}/Series/metadata?seriesId={}", kavita_api_url.clone(), manga_id.clone());
+	let metadata_url = format!("{}/Series/metadata?seriesId={}", kavita_api_url, manga_id);
 	let metadata_req = Request::get(encode_uri(&metadata_url))
 		.header("Authorization", get_kavita_api_auth().as_str());
 
@@ -202,30 +202,30 @@ fn get_manga_details(manga_id: String) -> Result<Manga> {
 	let metadata = metadata_resp.as_object().unwrap();
 
 	let mut authors = Vec::new();
-	for author in metadata.clone().get("pencillers").as_array().unwrap() {
+	for author in metadata.get("pencillers").as_array().unwrap() {
 		authors.push(author.as_object().unwrap().get("name").as_string().unwrap().to_string());
 	}
 
 	let mut artists = Vec::new();
-	for artist in metadata.clone().get("writers").as_array().unwrap() {
+	for artist in metadata.get("writers").as_array().unwrap() {
 		artists.push(artist.as_object().unwrap().get("name").as_string().unwrap().to_string());
 	}
 
 	let mut categories = Vec::new();
-	for category in metadata.clone().get("genres").as_array().unwrap() {
+	for category in metadata.get("genres").as_array().unwrap() {
 		categories.push(category.as_object().unwrap().get("title").as_string().unwrap().to_string());
 	}
-	for category in metadata.clone().get("tags").as_array().unwrap() {
+	for category in metadata.get("tags").as_array().unwrap() {
 		categories.push(category.as_object().unwrap().get("title").as_string().unwrap().to_string());
 	}
 
 	Ok(Manga {
 		id: manga_id.clone(),
-		cover: format!("{}/image/series-cover?seriesId={}&apiKey={}", kavita_api_url.clone(), manga_id.clone(), kavita_api_key.clone()),
+		cover: format!("{}/image/series-cover?seriesId={}&apiKey={}", kavita_api_url, manga_id, kavita_api_key),
 		title: series.get("name").as_string().unwrap().to_string(),
 		author: authors.join(", ").to_string(),
 		artist: artists.join(", ").to_string(),
-		description: metadata.clone().get("summary").as_string().unwrap().to_string(),
+		description: metadata.get("summary").as_string().unwrap().to_string(),
 		categories: categories,
 		status: match metadata.get("publicationStatus").as_int().unwrap() {
 			0 => MangaStatus::Ongoing,
@@ -242,7 +242,7 @@ fn get_manga_details(manga_id: String) -> Result<Manga> {
 fn get_chapter_list(manga_id: String) -> Result<Vec<Chapter>> {
 	let kavita_api_url = get_kavita_api_url();
 
-	let request_url = format!("{}/Series/volumes?seriesId={}", kavita_api_url.clone(), manga_id.clone());
+	let request_url = format!("{}/Series/volumes?seriesId={}", kavita_api_url, manga_id);
 	let request = Request::get(encode_uri(&request_url))
 		.header("Authorization", get_kavita_api_auth().as_str());
 
@@ -258,15 +258,15 @@ fn get_chapter_list(manga_id: String) -> Result<Vec<Chapter>> {
 	let mut result = Vec::new();
 	for volume_object in response.as_array().unwrap() {
 		let volume = volume_object.as_object().unwrap();
-		let volume_number = volume.clone().get("name").as_string().unwrap().to_string().parse::<f32>().unwrap();
+		let volume_number = volume.get("name").as_string().unwrap().to_string().parse::<f32>().unwrap();
 
 		for chapter_object in volume.get("chapters").as_array().unwrap() {
 			let chapter = chapter_object.as_object().unwrap();
-			let id = chapter.clone().get("id").as_int().unwrap().to_string();
-			let title = chapter.clone().get("titleName").as_string().unwrap().to_string();
-			let chapter_number = chapter.clone().get("number").as_string().unwrap().to_string().parse::<f32>().unwrap();
+			let id = chapter.get("id").as_int().unwrap().to_string();
+			let title = chapter.get("titleName").as_string().unwrap().to_string();
+			let chapter_number = chapter.get("number").as_string().unwrap().to_string().parse::<f32>().unwrap();
 
-			//let chapter_read = chapter.clone().get("pagesRead").as_string().unwrap().to_string().parse::<i32>().unwrap();
+			//let chapter_read = chapter.get("pagesRead").as_string().unwrap().to_string().parse::<i32>().unwrap();
 			let chapter_pages = chapter.get("pages").as_int().unwrap();
 			let chapter_special = chapter.get("isSpecial").as_bool().unwrap();
 
@@ -311,7 +311,7 @@ fn get_chapter_list(manga_id: String) -> Result<Vec<Chapter>> {
 fn get_page_list(_: String, chapter_id: String) -> Result<Vec<Page>> {
 	let kavita_api_url = get_kavita_api_url();
 	let kavita_api_key = get_kavita_api_key();
-	let request_url = format!("{}/Series/chapter?chapterId={}", kavita_api_url.clone(), chapter_id.clone());
+	let request_url = format!("{}/Series/chapter?chapterId={}", kavita_api_url, chapter_id);
 
 	let request = Request::get(encode_uri(&request_url))
 		.header("Authorization", get_kavita_api_auth().as_str());
@@ -328,7 +328,7 @@ fn get_page_list(_: String, chapter_id: String) -> Result<Vec<Page>> {
 	let page_number = response.as_object().unwrap().get("pages").as_int().unwrap();
 	Ok((0..page_number).map(|i| Page {
 		index: i as i32 + 1,
-		url: format!("{}/Reader/image?chapterId={}&page={}&apiKey={}&extractPdf=true", kavita_api_url.clone(), chapter_id, i, kavita_api_key.clone()),
+		url: format!("{}/Reader/image?chapterId={}&page={}&apiKey={}&extractPdf=true", kavita_api_url, chapter_id, i, kavita_api_key),
 		..Default::default()
 	}).collect())
 }
